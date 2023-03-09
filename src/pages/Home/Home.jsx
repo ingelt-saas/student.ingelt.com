4be1394ede60d7c6d6d2f5cb683f3c919974e1b8
Nishant Sharma <nishant-ai@outlook.com>
2023-03-09 18:33:36 +0530

    Updation Test

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
               />
 
               <h1 className="text-3xl font-semibold text-right py-5 px-5">
-                Welcome Harshita
+                Welcome Harshita!
               </h1>
             </div>
 
