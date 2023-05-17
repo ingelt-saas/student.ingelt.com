2cd3b29ab36b3b24c230e42d7b7f2aa19aceaf05
Palash <palashnath880@gmail.com>
2023-05-17 08:55:19 +0600

    Cookie Name Updated

--- src/api/config/axios.js
+++ src/api/config/axios.js
@@ -5,7 +5,7 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${Cookies.get('auth_token')}`
+    Authorization: `Bearer ${Cookies.get('student_auth_token')}`
   },
 });
 
