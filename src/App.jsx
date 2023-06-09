a8338cd89a73db9c933d4005f3b42a25128faeb9
Nishant Sharma <nishant-ai@outlook.com>
2023-06-09 17:27:21 +0530

    Socket to Message Integration

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@
 import { useEffect } from "react";
 import StudentProvider from "./providers/StudentProvider";
 import { ProSidebarProvider } from "react-pro-sidebar";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import OTPlessSdk from "otpless-js-sdk";
+import { SocketContext, socket } from "./contexts";
 
 // Routes
 import Routes from "./routes/Routes";
@@ -11,13 +12,10 @@ import { Worker } from "@react-pdf-viewer/core";
 import { ToastContainer } from "react-toastify";
 
 const App = () => {
-
-  const sdkInstance = new OTPlessSdk(
-    {
-      appId: "123456789",
-      enableErrorLogging: true
-    }
-  );
+  const sdkInstance = new OTPlessSdk({
+    appId: "123456789",
+    enableErrorLogging: true,
+  });
 
   useEffect(() => {
     if (localStorage.theme === "dark") {
@@ -26,25 +24,30 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    ;
     const sdk = sdkInstance.getState();
-    console.log(sdk)
+    console.log(sdk);
   }, [sdkInstance]);
 
   return (
     <ProSidebarProvider>
-      <StudentProvider>
-        <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.3.122/build/pdf.worker.min.js">
-          {/* <button id="whatsapp-login" className="bg-white" onClick={() => {
+      <SocketContext.Provider value={socket}>
+        <StudentProvider>
+          <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.3.122/build/pdf.worker.min.js">
+            {/* <button id="whatsapp-login" className="bg-white" onClick={() => {
             sdkInstance.createGetIntentOnClick({
               redirectionURL: 'http://localhost:3000'
             })
           }}>OTP</button> */}
-          <Routes />
-          <ToastContainer className='!text-sm' bodyClassName='!my-0' toastClassName='!min-h-fit !py-3' />
-        </Worker>
-      </StudentProvider >
-    </ProSidebarProvider >
+            <Routes />
+            <ToastContainer
+              className="!text-sm"
+              bodyClassName="!my-0"
+              toastClassName="!min-h-fit !py-3"
+            />
+          </Worker>
+        </StudentProvider>
+      </SocketContext.Provider>
+    </ProSidebarProvider>
   );
 };
 
