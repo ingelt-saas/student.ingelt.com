dc4c9a2164315476891a7c322e1eff027656cc5b
Palash <palashnath880@gmail.com>
2023-06-14 14:24:05 +0600

    Email Check API route updated

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,7 +2,7 @@ import instance from './config/axios';
 
 const authApi = {
     login: (data) => instance.post('/auth', data),
-    emailCheck: (email) => instance.post('/auth/email-check', email), // email checking before sign up
+    emailCheck: (email) => instance.post('/auth/emailCheck', email), // email checking before sign up
     signUp: (data) => instance.post('/auth/signup', data), //sign up
     resetEmail: (data) => instance.post('/auth/resetEmail', data),
     resetTokenVerify: (data) => instance.post('/auth/resetTokenVerify', data),
