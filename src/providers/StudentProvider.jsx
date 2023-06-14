a36f1bdb7de016e6e05ed554941ab1126a3f8b66
Palash <palashnath880@gmail.com>
2023-06-14 10:39:23 +0600

    Cookie Domain Updated

--- src/providers/StudentProvider.jsx
+++ src/providers/StudentProvider.jsx
@@ -12,7 +12,7 @@ const StudentProvider = ({ children }) => {
     // Shift to LocalStorage from Cookies
     // localStorage.removeItem("student_auth_token");
     // Cookies.remove("student_auth_token", { path: "/", domain: "" });
-    Cookies.remove("student_auth_token", { domain: 'ingelt.com' });
+    Cookies.remove("student_auth_token", { domain: '.ingelt.com' });
     window.location.pathname = "/";
 
   };
